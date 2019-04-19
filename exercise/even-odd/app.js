138570fa270fe006711c5df158cbb540e080286c
chans7692 <chans7692@gmail.com>
2019-04-19 09:16:35 -0600

    Day Three

---
+++ exercise/even-odd/app.js
@@ -0,0 +1,20 @@
+
+
+for (i = 0; i <= 101; i++) {
+    if (i % 2) {
+        console.log(i + " odd")
+    } else {
+        for(i = 0; i < 10; i++){
+            console.log(i)
+        }
+        
+        for(i = 10; i > 0; i--){
+            console.log(i)
+        }
+        
+        
+        var fruit = ["banana", "orange", "apple", "kiwi"]
+        
+        for(i = 0; i < fruit.length; i++){
+            console.log(fruit[i])
+        }
